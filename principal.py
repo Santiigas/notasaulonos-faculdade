25c199a73c5375765aea04e4e3f049a9c0c8050f
Santiigas <santiagosof@gmail.com>
2023-04-15 19:12:58 -0300

    Update principal.py
    Atualizando a insersão de dados na tabela aluno

--- principal.py
+++ principal.py
@@ -3,7 +3,7 @@ import random
 import string
 matricula = ''.join(random.choices(string.digits, k=12))
 
-conexao = conector.connect('C:/Users/Usuario/Documents/teste/meubanco11.db')
+conexao = conector.connect('C:/Users/Usuario/Documents/teste/meubanco12.db')
 conexao.execute("PRAGMA foreign_keys = on")
 cursor = conexao.cursor()
 
@@ -39,41 +39,31 @@ escolha = int(input('-- Escolha uma opção:'))
 if escolha == 1:
     while True:
         nome_aluno = str(input('Qual o nome do aluno?'))
-        nota1 = float(input("Informe a primeira nota:"))
-        nota2 = float(input("Informe a segunda nota:"))
-        nota3 = float(input("Informe a terceira nota:"))
-        media = (nota1 + nota2 + nota3) / 3
+        cpf_aluno = int(input("Informe o CPF do aluno:"))
         if len(nome_aluno) > 0 and nome_aluno.isalpha():
-            try:
-                comando = '''INSERT INTO aluno VALUES (:cpf_aluno, :nome_aluno, :matricula);'''
-                cursor.execute(comando, {"cpf_aluno": 122222, "nome_aluno": nome_aluno, "matricula": matricula})
-                conexao.commit()
-
-                comando = '''INSERT INTO disciplina VALUES (":id_disciplina", ":nome_disciplina");'''
-                cursor.execute(comando, {"id_disciplina": 5555, "nome_disciplina": "eusanti"})
-                conexao.commit()
-
-                comando = '''INSERT INTO resultados VALUES (":nota1, :nota2, :nota3, :media);'''
-                cursor.execute(comando, {"nota1":nota1, "nota2":nota2, "nota3":nota3, "media": media})
-                conexao.commit()
-
-                break    
-            except Exception as erro:
-                print(erro)
+                try:
+                    comando = '''INSERT INTO aluno VALUES (:cpf_aluno, :nome_aluno, :matricula);'''
+                    cursor.execute(comando, {"cpf_aluno": cpf_aluno, "nome_aluno": nome_aluno, "matricula": matricula})
+                    conexao.commit()
+                    print(">>>> Dados adicionados com sucesso!")
+                    break    
+                except Exception as erro:
+                    print(erro)
         else:
             print('>>> Valores digitados invalidos! Tente novamente')               
 elif escolha == 2:
     while True:
         cpf_aluno = float(input("Informe o CPF do aluno a ser deletado:"))
-        if len(cpf_aluno) == 11:
-            try:
-                comando = '''DELETE FROM aluno WHERE cpf_aluno = :cpf_aluno;'''
-                cursor.execute(comando, {"cpf_aluno": cpf_aluno})
+        try:
+            comando = '''DELETE FROM aluno WHERE cpf_aluno = :cpf_aluno;'''
+            cursor.execute(comando, {"cpf_aluno": cpf_aluno})
+            conexao.commit()
+            print(">>> Dados apagados com sucesso!")
+            break
 
-                conexao.commit() 
-            except Exception as erro:
-                print(erro)         
-        print('>>> Valores digitados invalidos! Tente novamente')  
+        except Exception as erro:
+            print(erro)         
+            print('>>> Valores digitados invalidos! Tente novamente')  
 elif escolha == 3:
     while True:
         nome_aluno = str(input('Qual o nome do aluno?'))
@@ -82,7 +72,9 @@ elif escolha == 3:
                 comando = '''UPDATE aluno SET nome_aluno = :nome_aluno;'''
                 cursor.execute(comando, {"nome_aluno": nome_aluno})
                 conexao.commit() 
+                print(">>> Dados atualizados com sucesso!")
                 break
+
             except Exception as erro:
                 print(erro)
         else:
