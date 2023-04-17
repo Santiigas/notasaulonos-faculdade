8fc8110aca5f6df1830bb209cbe7f3187ce889bf
Santiigas <santiagosof@gmail.com>
2023-04-17 09:25:40 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -3,15 +3,14 @@ import random
 import string
 matricula = ''.join(random.choices(string.digits, k=12))
 
-conexao = conector.connect('C:/Users/Usuario/Documents/teste/meubanco13.db')
+conexao = conector.connect('C:/Users/Usuario/Documents/teste/meubanco16.db')
 conexao.execute("PRAGMA foreign_keys = on")
 cursor = conexao.cursor()
 
 try:
     cursor.execute('''CREATE TABLE aluno (
-                    cpf_aluno  INTEGER   PRIMARY KEY,
-                    nome_aluno TEXT (50),
-                    matricula  INTEGER);''')
+                    matricula  INTEGER   PRIMARY KEY,
+                    nome_aluno TEXT (50);''')
 
 
     cursor.execute('''CREATE TABLE disciplina (
@@ -20,7 +19,7 @@ try:
 
                         
     cursor.execute('''CREATE TABLE resultados (
-                    aluno_id       INTEGER REFERENCES aluno (cpf_aluno),
+                    aluno_id       INTEGER REFERENCES aluno (matricula),
                     disciplina_id INTEGER REFERENCES disciplina (id_disciplina),
                     nota1                    REAL,
                     nota2                    REAL,
@@ -38,11 +37,10 @@ while True:
     if escolha == 1:
         while True:
             nome_aluno = str(input('Qual o nome do aluno?'))
-            cpf_aluno = int(input("Informe o CPF do aluno:"))
             if len(nome_aluno) > 0 and nome_aluno.isalpha():
                     try:
-                        comando = '''INSERT INTO aluno VALUES (:cpf_aluno, :nome_aluno, :matricula);'''
-                        cursor.execute(comando, {"cpf_aluno": cpf_aluno, "nome_aluno": nome_aluno, "matricula": matricula})
+                        comando = '''INSERT INTO aluno VALUES (:matricula, :nome_aluno);'''
+                        cursor.execute(comando, {"matricula": matricula, "nome_aluno": nome_aluno})
                         conexao.commit()
                         print(">>>> Dados adicionados com sucesso!")
                         break    
@@ -52,10 +50,10 @@ while True:
                 print('>>> Valores digitados invalidos! Tente novamente')               
     elif escolha == 2:
         while True:
-            cpf_aluno = float(input("Informe o CPF do aluno a ser deletado:"))
+            matricula = float(input("Informe a matricula do aluno a ser deletado:"))
             try:
-                comando = '''DELETE FROM aluno WHERE cpf_aluno = :cpf_aluno;'''
-                cursor.execute(comando, {"cpf_aluno": cpf_aluno})
+                comando = '''DELETE FROM aluno WHERE matricula = :matricula;'''
+                cursor.execute(comando, {"matricula": matricula})
                 conexao.commit()
                 print(">>> Dados apagados com sucesso!")
                 break
@@ -64,11 +62,11 @@ while True:
                 print('>>> Valores digitados invalidos! Tente novamente')  
     elif escolha == 3:
         while True:
-            cpf_aluno = float(input("Informe o CPF do aluno a ser alterado:"))
+            matricula = float(input("Informe a matricula do aluno a ser alterado:"))
             nome_aluno = str(input('Qual será o novo nome?'))
             if len(nome_aluno) > 0 and nome_aluno.isalpha():    
                 try:
-                    cursor.execute("UPDATE aluno SET nome_aluno = ? WHERE cpf_aluno = ?", (nome_aluno, cpf_aluno))
+                    cursor.execute("UPDATE aluno SET nome_aluno = ? WHERE matricula = ?", (nome_aluno, matricula))
                     conexao.commit() 
                     print(">>> Dados atualizados com sucesso!")
                     break
@@ -84,11 +82,3 @@ while True:
         print('Comando invalido! Tente Novamente ')
 cursor.close()
 conexao.close()
-
-'''Minha ideia no inicio era add todos os dados em todas as tabelas, mas estava dando muitos erros de chaves etc.
-Acredito que tenha encontrado a solução, mas resolvi deixar somente na tabela alunos.
-
-Outro ponto que está falando é colocar uma verificação de CPF por meio de uma função e add classes.
-Também irei trocar a chave primaria para matricula, já que está com o cpf, e isso a longo prazo não iria funcionar bem!
-
-Irei realizar mais atulizações durante a semana, mas acho que o que eu fiz já resolve a atividade haha, valeu professor'''
