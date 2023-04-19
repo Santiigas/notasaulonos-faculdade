56ebaaaeb77c4c573060a84f2db0471c2a8ef186
Santiigas <santiagosof@gmail.com>
2023-04-18 23:04:12 -0300

    Update principal.py
    Alterações ainda não testadas

--- principal.py
+++ principal.py
@@ -1,7 +1,6 @@
 import sqlite3 as conector
 import random
 import string
-matricula = ''.join(random.choices(string.digits, k=12))
 
 conexao = conector.connect('C:/Users/Usuario/Documents/teste/meubanco8.db')
 #conexao.execute("PRAGMA foreign_keys = on")
@@ -25,11 +24,62 @@ try:
                         media REAL);''')
 except Exception as erro:
     print(erro)
+
+class Aluno:
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
+        pass
+    
+    def InserirDados(self, nome_aluno, matricula, nome_disciplina, id_disciplina, nota1, nota2, nota3):
+        media = (nota1 + nota2 + nota3) / 3
+        try:
+            comando = '''INSERT INTO aluno VALUES (:matricula, :nome_aluno);'''
+            cursor.execute(comando, {"matricula": matricula, "nome_aluno": nome_aluno})
+            conexao.commit()
+        except Exception as erro:
+            print(erro)
+
+        try:
+            comando = '''INSERT INTO disciplina VALUES (:id_disciplina, :nome_disciplina);'''
+            cursor.execute(comando, {"id_disciplina": id_disciplina, "nome_disciplina": nome_disciplina})
+            conexao.commit()
+        except Exception as erro:
+            print(erro)
+
+        try:
+            comando = '''INSERT INTO resultados VALUES (:aluno_id, :disciplina_id, :nota1, :nota2, :nota3, :media);'''
+            cursor.execute(comando, {"aluno_id": matricula, "disciplina_id": id_disciplina, "nota1": nota1, "nota2": nota2, "nota3": nota3, "media": media})
+            conexao.commit()
+        except Exception as erro:
+            print(erro)
+
+    def ExcluirDados(self, matricula):
+        try:
+            comando = '''DELETE FROM aluno WHERE matricula = :matricula;'''
+            cursor.execute(comando, {"matricula": matricula})
+
+            comando = '''DELETE FROM resultados WHERE aluno_id = :aluno_id;'''
+            cursor.execute(comando, {"aluno_id": matricula})
+            conexao.commit()
+        except Exception as erro:
+            print(erro)         
+
+    def AlterarDados(self, matricula, nome_aluno):
+        try:
+            cursor.execute("UPDATE aluno SET nome_aluno = ? WHERE matricula = ?", (nome_aluno, matricula))
+            conexao.commit()
+        except Exception as erro:
+            print(erro)
+
+    def Finalizar(self):
+        pass
+
 while True:
     matricula = ''.join(random.choices(string.digits, k=12))
     id_disciplina = ''.join(random.choices(string.digits, k=6))
     print('*-' * 15)
-    print('---- Meu banco de dados ----')
+    print('\n---- Meu banco de dados ----')
     print('*-' * 15)
     print('>>> O que você quer fazer?\n[ 1 ] - Inserir dados\n[ 2 ] - Excluir\n[ 3 ] - Alterar\n[ 4 ] - Sair' )
     print('*-' * 15)
@@ -38,82 +88,35 @@ while True:
         while True:
             nome_aluno = str(input('Qual o nome do aluno?'))
             if len(nome_aluno) > 0 and nome_aluno.isalpha():
-                    try:
-                        comando = '''INSERT INTO aluno VALUES (:matricula, :nome_aluno);'''
-                        cursor.execute(comando, {"matricula": matricula, "nome_aluno": nome_aluno})
-                        conexao.commit()
-
-                        break    
-                    except Exception as erro:
-                        print(erro)
+                pass
             else:
                 print('-- Nome invalido! Tente novamente')   
-        while True:
             nome_disciplina = str(input('Nome da disciplina:'))
             if len(nome_disciplina) > 0 and nome_disciplina.isalpha():
-                try:
-                    comando = '''INSERT INTO disciplina VALUES (:id_disciplina, :nome_disciplina);'''
-                    cursor.execute(comando, {"id_disciplina": id_disciplina, "nome_disciplina": nome_disciplina})
-                    conexao.commit()
-
-                    break
-                except Exception as erro:
-                        print(erro)
+                pass
             else:
-                print('-- Disciplina invalida! Tente novamente') 
-        while True:
-            nota_1 = float(input('Informe a 1ª nota: '))
-            nota_2 = float(input('Informe a 2ª nota: '))
-            nota_3 = float(input('Informe a 3ª nota: '))
-            media = (nota_1 + nota_2 + nota_3) / 3
-            try:
-                comando = '''INSERT INTO resultados VALUES (:aluno_id, :disciplina_id, :nota1, :nota2, :nota3, :media);'''
-                cursor.execute(comando, {"aluno_id": matricula, "disciplina_id": id_disciplina, "nota1": nota_1, "nota2": nota_2, "nota3": nota_3, "media": media})
-                conexao.commit()
+                print('-- Disciplina invalida! Tente novamente')  
+            nota1 = float(input('Informe a 1ª nota: '))
+            nota2 = float(input('Informe a 2ª nota: '))
+            nota3 = float(input('Informe a 3ª nota: '))
+            Aluno.InserirDados(nome_aluno, matricula, nome_disciplina, id_disciplina, nota1, nota2, nota3)
 
-                print('><' * 12)
-                print(">>> Dados inseridos com sucesso!")
-                break
-            except Exception as erro:
-                print(erro)
     elif escolha == 2:
         while True:
             matricula = float(input("Informe a matricula do aluno a ser deletado:"))
-            try:
-                comando = '''DELETE FROM aluno WHERE matricula = :matricula;'''
-                cursor.execute(comando, {"matricula": matricula})
+            Aluno.ExcluirDados(matricula)
 
-                comando = '''DELETE FROM resultados WHERE aluno_id = :aluno_id;'''
-                cursor.execute(comando, {"aluno_id": matricula})
-                conexao.commit()
-
-                print('><' * 12)
-                print(">>> Dados apagados com sucesso!")
-                break
-            except Exception as erro:
-                print(erro)         
-                print('>>> Valores digitados invalidos! Tente novamente')  
     elif escolha == 3:
         while True:
             matricula = float(input("Informe a matricula do aluno a ser alterado:"))
             nome_aluno = str(input('Qual será o novo nome?'))
-            if len(nome_aluno) > 0 and nome_aluno.isalpha():    
-                try:
-                    cursor.execute("UPDATE aluno SET nome_aluno = ? WHERE matricula = ?", (nome_aluno, matricula))
-                    conexao.commit()
-
-                    print('><' * 12)
-                    print(">>> Dados atualizados com sucesso!")
-                    break
-                except Exception as erro:
-                    print(erro)
-            else:
-                print('>>> Valores digitados invalidos! Tente novamente') 
+            if len(nome_aluno) > 0 and nome_aluno.isalpha():
+                Aluno.AlterarDados(matricula, nome_aluno)
+                
     elif escolha == 4:
-        print("Encerrando...")
-        print('>>> Obrigado por utilizar!')
+        Aluno.Finalizar()
         break                     
     else:
-        print('Comando invalido! Tente Novamente ')
+        print('Comando invalido! Tente Novamente')
 cursor.close()
 conexao.close()
